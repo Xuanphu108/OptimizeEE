20cdaa4fe8926af0180f42f96545e27cdc91f7d7
Xuanphu108 <nxphu.1994@gmail.com>
2021-03-27 23:00:28 +0700

    update

--- Optimizer/BisectionSearch.py
+++ Optimizer/BisectionSearch.py
@@ -18,14 +18,14 @@ class BisectionSearch(object):
                  subBandwidth=78*10**3,
                  effPower=0.8,
                  powUserCir=10**(-3),
-                 powPScir=10**((5/10)-3),
+                 powPsCir=10**((5/10)-3),
                  dinkel=1):
         self.channelPsUsers = channelPsUsers
         self.channelApUsers = channelApUsers
         self.binaryMatrix = binaryMatrix
         self.noise = noise
         self.powUserCir = powUserCir
-        self.powPScir = powPScir
+        self.powPsCir = powPsCir
         self.effPower = effPower
         self.dinkel = dinkel
         self.T = 1
@@ -38,7 +38,7 @@ class BisectionSearch(object):
         channel = self.apUsers/(self.subBandwidth*self.noise)
         sumRate = (1-per)*self.epsilon*np.log(1 + np.multiply((self.effPower*power*per*psUsers/(1-per) - self.powUserCir),channel))
         sumRate = np.sum(sumRate)
-        X1 = per*(power - self.effPower*power*np.sum(psUsers) + self.powPScir)
+        X1 = per*(power - self.effPower*power*np.sum(psUsers) + self.powPsCir)
         X2 = (1-per)*np.sum(np.multiply((self.effPower*power*per*psUsers/(1-per)-self.powUserCir),self.binaryMatrix))
         obj = sumRate - self.dinkel*(X1 + X2)
         return obj 
@@ -55,15 +55,15 @@ class BisectionSearch(object):
         return powerDerivative
 
     def calcPerDerivative(self, power, per):
-        PS_users = self.channelPsUsers
+        psUsers = self.channelPsUsers
         channel = self.apUsers/(self.subBandwidth*self.noise)
-        A_per = 1 - self.powUserCir*channel
-        B_per = self.effPower*power*np.multiply(PS_users,channel)
-        X1_PerDer = power - self.effPower*power*np.sum(PS_users) + self.powPScir
-        X2_PerDer = np.sum(np.multiply((self.effPower*power*PS_users+self.powUserCir),self.binaryMatrix))
-        ValPerDer = np.sum(self.epsilon*np.log((1-per)/((B_per-A_per)*per+A_per)) + self.epsilon*(B_per-A_per)*(1-per)/((B_per-A_per)*per+A_per) + self.epsilon) - \
-                    self.dinkel*(X1_PerDer + X2_PerDer)
-        return ValPerDer
+        tempX1 = 1 - self.powUserCir*channel
+        tempX2 = self.effPower*power*np.multiply(psUsers,channel)
+        tempY1 = power - self.effPower*power*np.sum(psUsers) + self.powPsCir
+        tempY2 = np.sum(np.multiply((self.effPower*power*psUsers+self.powUserCir),self.binaryMatrix))
+        periodDerivative = np.sum(self.epsilon*np.log((1-per)/((tempX2-tempX1)*per+tempX1)) + self.epsilon*(tempX2-tempX1)*(1-per)/((tempX2-tempX1)*per+tempX1) + self.epsilon) - \
+                           self.dinkel*(tempY1 + tempY2)
+        return periodDerivative
 
     @staticmethod 
     def split_based(mat, val):
@@ -72,149 +72,149 @@ class BisectionSearch(object):
         out = np.array(list(map(list,temp)))
         return out
 
-    def initPower(self, Pmax, PerMax):
-        PS_users = self.channelPsUsers
+    def initPower(self, pMax, perMax):
+        psUsers = self.channelPsUsers
         channel = self.channelApUsers/(self.subBandwidth*self.noise)
         # Initialize power
-        PowerList = np.arange(0, Pmax, 1/1000000)
-        A_per = 1 - self.powUserCir*channel
-        B_per = self.effPower*np.outer(PowerList[1:],np.multiply(PS_users,channel))
-        B_per = B_per.reshape(len(PowerList[1:]), channel.shape[0], channel.shape[1])
-        C_per = (B_per-A_per)
-        A_per = np.repeat(A_per[np.newaxis,...], len(PowerList[1:]), axis=0)
+        powerList = np.arange(0, pMax, 1/1000000)
+        tempA = 1 - self.powUserCir*channel
+        tempB = self.effPower*np.outer(powerList[1:],np.multiply(psUsers,channel))
+        tempB = tempB.reshape(len(powerList[1:]), channel.shape[0], channel.shape[1])
+        tempC = (tempB-tempA)
+        tempA = np.repeat(tempA[np.newaxis,...], len(powerList[1:]), axis=0)
 
-        C_neg = C_per.clip(max=0)
-        C_pos = C_per.clip(min=0)
+        negC = tempC.clip(max=0)
+        posC = tempC.clip(min=0)
 
-        UpperBound = np.divide(-A_per, C_neg, out = np.zeros_like(-A_per), where=C_neg!=0)
-        UpperBound = UpperBound.reshape(len(PowerList[1:]), channel.shape[0]*channel.shape[1])
-        UpperBound[UpperBound==0] = +inf
-        UpperBound = np.min(UpperBound, axis=1)
-        UpperBound = np.minimum(UpperBound, PerMax-0.0000000000001)
+        upperBound = np.divide(-tempA, negC, out = np.zeros_like(-tempA), where=negC!=0)
+        upperBound = upperBound.reshape(len(powerList[1:]), channel.shape[0]*channel.shape[1])
+        upperBound[upperBound==0] = +inf
+        upperBound = np.min(upperBound, axis=1)
+        upperBound = np.minimum(upperBound, perMax-0.0000000000001)
         
-        LowerBound = np.divide(-A_per, C_pos, out = np.zeros_like(-A_per), where=C_pos!=0)
-        LowerBound = LowerBound.reshape(len(PowerList[1:]), channel.shape[0]*channel.shape[1])
-        # LowerBound = self.split_based(LowerBound, 1) # Remove 1
-        LowerBound = np.max(LowerBound, axis=1)
-        LowerBound = np.maximum(LowerBound, 0.0000000000001)
+        lowerBound = np.divide(-tempA, posC, out = np.zeros_like(-tempA), where=posC!=0)
+        lowerBound = lowerBound.reshape(len(powerList[1:]), channel.shape[0]*channel.shape[1])
+        # lowerBound = self.split_based(lowerBound, 1) # Remove 1
+        lowerBound = np.max(lowerBound, axis=1)
+        lowerBound = np.maximum(lowerBound, 0.0000000000001)
 
-        TrueList = np.where(LowerBound <= UpperBound)
-        if len(TrueList[0]) == 0:
+        trueList = np.where(lowerBound <= upperBound)
+        if len(trueList[0]) == 0:
             raise ValueError('Parameters is not suitable. Please adjust the parameters!')
-        PowerInit = PowerList[1:][TrueList[0][int(len(TrueList)/2)]]
-        LowerBoundInit = LowerBound[TrueList[0][int(len(TrueList)/2)]]
-        return PowerInit, LowerBoundInit
+        initPower = powerList[1:][trueList[0][int(len(trueList)/2)]]
+        initLowerBound = lowerBound[trueList[0][int(len(trueList)/2)]]
+        return initPower, initLowerBound
 
-    def initPeriod(self, Pmax, PerMax):
-        PS_users = self.channelPsUsers
+    def initPeriod(self, pMax, perMax):
+        psUsers = self.channelPsUsers
         channel = self.channelApUsers/(self.subBandwidth*self.noise)
         # Initialize period
-        PeriodList = np.arange(0, PerMax, 1/1000000)
-        A_pow = 1 - self.powUserCir*channel
-        B_pow = self.effPower*np.outer(PeriodList[1:]/(1-PeriodList[1:]),np.multiply(PS_users,channel))
-        B_pow = B_pow.reshape(len(PeriodList[1:]), channel.shape[0], channel.shape[1])
-        A_pow = np.repeat(A_pow[np.newaxis,...], len(PeriodList[1:]), axis=0)
+        periodList = np.arange(0, perMax, 1/1000000)
+        tempA = 1 - self.powUserCir*channel
+        tempB = self.effPower*np.outer(periodList[1:]/(1-periodList[1:]),np.multiply(psUsers,channel))
+        tempB = tempB.reshape(len(periodList[1:]), channel.shape[0], channel.shape[1])
+        tempA = np.repeat(tempA[np.newaxis,...], len(periodList[1:]), axis=0)
 
-        B_neg = B_pow.clip(max=0)
-        B_pos = B_pow.clip(min=0)
+        negB = tempB.clip(max=0)
+        posB = tempB.clip(min=0)
 
-        UpperBound = np.divide(-A_pow, B_neg, out = np.zeros_like(-A_pow), where=B_neg!=0)
-        UpperBound = UpperBound.reshape(len(PeriodList[1:]), channel.shape[0]*channel.shape[1])
-        UpperBound[UpperBound==0] = +inf
-        UpperBound = np.min(UpperBound, axis=1)
-        UpperBound = np.minimum(UpperBound, Pmax)
+        upperBound = np.divide(-tempA, negB, out = np.zeros_like(-tempA), where=B_neg!=0)
+        upperBound = upperBound.reshape(len(periodList[1:]), channel.shape[0]*channel.shape[1])
+        upperBound[upperBound==0] = +inf
+        upperBound = np.min(upperBound, axis=1)
+        upperBound = np.minimum(upperBound, pMax)
         
-        LowerBound = np.divide(-A_pow, B_pos, out=np.zeros_like(-A_pow), where=B_pos!=0)
-        LowerBound = LowerBound.reshape(len(PeriodList[1:]), channel.shape[0]*channel.shape[1])
-        LowerBound = np.max(LowerBound, axis=1)
-        LowerBound = np.maximum(LowerBound, 0.0000000000001)
-        TrueList = np.where(LowerBound <= UpperBound)
-        if len(TrueList[0]) == 0:
+        lowerBound = np.divide(-tempA, posB, out=np.zeros_like(-tempA), where=posB!=0)
+        lowerBound = lowerBound.reshape(len(periodList[1:]), channel.shape[0]*channel.shape[1])
+        lowerBound = np.max(lowerBound, axis=1)
+        lowerBound = np.maximum(lowerBound, 0.0000000000001)
+        trueList = np.where(lowerBound <= upperBound)
+        if len(trueList[0]) == 0:
             raise ValueError('Parameters is not suitable. Please adjust the parameters!')
         # import ipdb; ipdb.set_trace()
-        PeriodInit = PeriodList[1:][TrueList[0][int(len(TrueList)/2)]]
-        LowerBoundInit = LowerBound[TrueList[0][int(len(TrueList)/2)]]
-        return PeriodInit, LowerBoundInit
+        initPeriod = periodList[1:][trueList[0][int(len(trueList)/2)]]
+        initLowerBound = lowerBound[trueList[0][int(len(trueList)/2)]]
+        return initPeriod, initLowerBound
         
-    def searchPower(self, Pmax, PerMax, per, OldPower, index):
-        PS_users = self.channelPsUsers
+    def searchPower(self, pMax, perMax, per, oldPower, index):
+        psUsers = self.channelPsUsers
         # channel = self.apUsers/(self.subBandwidth*self.noise)
         channel = self.channelApUsers/(self.subBandwidth*self.noise)
-        A_pow = 1 - self.powUserCir*channel
-        B_pow = self.effPower*np.multiply(PS_users,channel)*per/(1-per)
-        B_neg = B_pow.clip(max=0)
-        B_pos = B_pow.clip(min=0)
+        tempA = 1 - self.powUserCir*channel
+        tempB = self.effPower*np.multiply(psUsers,channel)*per/(1-per)
+        negB = tempB.clip(max=0)
+        posB = tempB.clip(min=0)
         delta = 0.00000000000001 # 0.000000000000001  # Standard: 5
-        UpperBound = np.divide(-A_pow, B_neg, out = np.zeros_like(-A_pow), where=B_neg!=0)
-        UpperBound[UpperBound==0] = +inf
-        UpperBound = np.min(UpperBound)
-        UpperBound = np.minimum(UpperBound, Pmax)
+        upperBound = np.divide(-tempA, negB, out = np.zeros_like(-tempA), where=negB!=0)
+        upperBound[upperBound==0] = +inf
+        upperBound = np.min(upperBound)
+        upperBound = np.minimum(upperBound, pMax)
 
-        LowerBound = np.divide(-A_pow, B_pos, out=np.zeros_like(-A_pow), where=B_pos!=0)
-        LowerBound = np.max(LowerBound)
-        # LowerBound = np.maximum(LowerBound, 0.0000000000001)
+        lowerBound = np.divide(-tempA, posB, out=np.zeros_like(-tempA), where=posB!=0)
+        lowerBound = np.max(lowerBound)
+        # lowerBound = np.maximum(lowerBound, 0.0000000000001)
                 
         if index == 1:
-            if (UpperBound - LowerBound) <= delta:
+            if (upperBound - lowerBound) <= delta:
                 print('Starting point (period) is BAD!')
-                per, LowerBound  = self.initPeriod(Pmax, PerMax)
+                per, lowerBound  = self.initPeriod(pMax, perMax)
             else:
                 print('Starting point (period) is GOOD!')
                 
-        if (UpperBound - LowerBound) <= delta:
-            power = OldPower 
+        if (upperBound - lowerBound) <= delta:
+            power = oldPower 
         else: 
-            while UpperBound - LowerBound > delta:
-                power = (UpperBound + LowerBound)/2
+            while upperBound - lowerBound > delta:
+                power = (upperBound + lowerBound)/2
                 if self.calcPowDerivative(power, per) > 0:
-                    LowerBound = power
+                    lowerBound = power
                 else:
-                    UpperBound = power
-        OptObjective = self.calcObject(power, per)
-        return power, OptObjective
+                    upperBound = power
+        optObjective = self.calcObject(power, per)
+        return power, optObjective
 
-    def searchPeriod(self, Pmax, PerMax, power, OldPer, index):
-        PS_users = self.channelPsUsers
+    def searchPeriod(self, pMax, perMax, power, oldPer, index):
+        psUsers = self.channelPsUsers
         # channel = self.apUsers/(self.subBandwidth*self.noise)
         channel = self.channelApUsers/(self.subBandwidth*self.noise)
-        A_per = 1 - self.powUserCir*channel
-        B_per = self.effPower*power*np.multiply(PS_users, channel)
-        C_per = (B_per-A_per)
-        C_neg = C_per.clip(max=0)
-        C_pos = C_per.clip(min=0)
+        tempA = 1 - self.powUserCir*channel
+        tempB = self.effPower*power*np.multiply(psUsers, channel)
+        tempC = (tempB-tempA)
+        negC = tempC.clip(max=0)
+        posC = tempC.clip(min=0)
         delta = 0.00000000000001 # 0.000000000000001 # Standard: 5
 
-        UpperBound = np.divide(-A_per, C_neg, out = np.zeros_like(-A_per), where=C_neg!=0)
-        UpperBound[UpperBound==0] = +inf
-        UpperBound = np.min(UpperBound)
-        UpperBound = np.minimum(UpperBound, PerMax-0.0000000000001)
+        upperBound = np.divide(-tempA, negC, out = np.zeros_like(-tempA), where=negC!=0)
+        upperBound[upperBound==0] = +inf
+        upperBound = np.min(upperBound)
+        upperBound = np.minimum(upperBound, perMax-0.0000000000001)
         
-        LowerBound = np.divide(-A_per, C_pos, out = np.zeros_like(-A_per), where=C_pos!=0)
-        LowerBound = LowerBound.flatten()
-        LowerBound = np.delete(LowerBound, np.where(LowerBound==1))
-        LowerBound = np.max(LowerBound)
-        LowerBound = np.maximum(LowerBound, 10**(-2))
-        # LowerBound = np.maximum(LowerBound,0.0000000000001)
+        lowerBound = np.divide(-tempA, posC, out = np.zeros_like(-tempA), where=posC!=0)
+        lowerBound = lowerBound.flatten()
+        lowerBound = np.delete(lowerBound, np.where(lowerBound==1))
+        lowerBound = np.max(lowerBound)
+        lowerBound = np.maximum(lowerBound, 10**(-2))
+        # lowerBound = np.maximum(lowerBound,0.0000000000001)
 
         # import ipdb; ipdb.set_trace()       
         if index == 1:
-            if (UpperBound - LowerBound) <= delta:
+            if (upperBound - lowerBound) <= delta:
                 print('Starting point (power) is BAD!')
-                power, LowerBound  = self.initPower(Pmax, PerMax)
+                power, lowerBound  = self.initPower(pMax, perMax)
             else:
                 print('Starting point (power) is GOOD!')
 
-        if (UpperBound - LowerBound) <= delta:
-            per = OldPer
+        if (upperBound - lowerBound) <= delta:
+            per = oldPer
         else:
-            while UpperBound - LowerBound > delta:
-                per = (UpperBound + LowerBound)/2
+            while upperBound - lowerBound > delta:
+                per = (upperBound + lowerBound)/2
                 if self.calcPerDerivative(power, per) > 0:
-                    LowerBound = per
+                    lowerBound = per
                 else:
-                    UpperBound = per
-        OptObjective = self.calcObject(power, per)
-        return per, OptObjective
+                    upperBound = per
+        optObjective = self.calcObject(power, per)
+        return per, optObjective
 
 
 if __name__ == "__main__":
@@ -222,7 +222,7 @@ if __name__ == "__main__":
     subBandwidth=78*10**3
     effPower=0.8
     powUserCir=10**(-3)
-    powPScir=10**((5/10)-3)
+    powPsCir=10**((5/10)-3)
     dinkel= 10.804231578308528 # 0.5
     binaryMatrix = np.array([[0, 0, 0, 0, 0, 0], 
                              [1, 1, 1, 1, 1, 1], 
@@ -235,20 +235,20 @@ if __name__ == "__main__":
                                  [13,14,15,16,17,18],
                                  [19,20,21,22,23,24]])
     start_time = time.time()
-    power, OptObjective = BisectionSearch(channelPsUsers,
+    power, optObjective = BisectionSearch(channelPsUsers,
                                           channelApUsers,
                                           binaryMatrix,
                                           noise,
                                           subBandwidth,
                                           effPower,
                                           powUserCir,
-                                          powPScir,
+                                          powPsCir,
                                           dinkel).searchPower(0.3004101332156862)
                                 
     total_time = time.time() - start_time
     print('Optimization time: %f' %total_time)
     print('Power: %f' %power)
-    print('Objective: %f' %OptObjective)
+    print('Objective: %f' %optObjective)
 
 
 
